b81f048daf1d3fa6eed94e06cb8580cb6eb64e3d
DaanHeijnen <38262097+DaanHeijnen@users.noreply.github.com>
2026-06-22 15:57:57 +0200

    Increase wheelPxPerZoomLevel from 90 to 120

--- dist/src/main.js
+++ dist/src/main.js
@@ -538,7 +538,7 @@ function initMap() {
     scrollWheelZoom: true,
     zoomSnap: 1,
     zoomDelta: 1,
-    wheelPxPerZoomLevel: 90,
+    wheelPxPerZoomLevel: 120,
     wheelDebounceTime: 45,
     zoomAnimation: false,
     fadeAnimation: false,
